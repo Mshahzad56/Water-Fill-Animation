b2482bae5c8bf5b7f2458e0c26cabb86f7d0e34e
Malik Shahazad <m.shahzad.mail.56@gmail.com>
2023-08-11 22:54:01 +0500

    fill

---
+++ main.js
@@ -0,0 +1,34 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const listers = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
+
+
+updateBigCup()
+
+smallCups.forEach((cup, idx)=>{
+    cup.addEventListener('click', () => heightlightCups(idx))
+})
+
+function heightlightCups(idx){
+
+    if(smallCups[idx]. classList.contains('full') && !smallCups
+    [idx].nextElementSibling.classList.contains('full')){
+        idx--
+
+    }
+
+    smallCups.forEach((cup, idx2) =>{
+        if(idx2 <= idx){
+            cup.classList.add('full')
+        }else{
+            cup.classList.remove('full')
+        }
+    })
+    updateBigCup()
+}
+function updateBigCup(){
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    
+}
+
